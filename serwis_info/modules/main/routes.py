b763f61aba48b1794f31c8f61dbebd3ccf89d729
gorsz <gorsz@LAPGOS>
2025-11-16 15:48:29 +0100

    Strona główna

---
+++ serwis_info/modules/main/routes.py
@@ -0,0 +1,14 @@
+from flask import Blueprint, render_template
+
+main_bp = Blueprint(
+    "main",
+    __name__,
+    template_folder="templates",
+    static_folder="static",         
+    static_url_path="/main_static"  
+)
+
+
+@main_bp.route("/")
+def index():
+    return render_template("index.html", body_class="home-page")
