8359173e8c7e4fad70c89d5c0f7c4e3035924f2e
Marlena <marlenaw@student.agh.edu.pl>
2025-11-17 11:53:47 +0100

    zmiany

--- serwis_info/create_app.py
+++ serwis_info/create_app.py
@@ -1,15 +1,27 @@
-from flask import Flask
+from flask import Flask, render_template
 
 def create_app():
-    app = Flask(__name__)
+    app = Flask(__name__, template_folder="templates", static_folder="static")
     
-    # Tutaj możesz dodawać konfigurację globalną
-    app.config['SECRET_KEY'] = 'tajny_klucz'
+    from serwis_info.modules.exchange.routes.currencies import currencies_bp
+    from serwis_info.modules.exchange.routes.stockmarket import stockmarket_bp
+    from serwis_info.modules.exchange.routes.journey import journey_bp    
+    from serwis_info.modules.main.routes.main import main_bp
+    from serwis_info.modules.main.routes.auth import auth_bp
+    
+    app.secret_key = "moja"
+    app.register_blueprint(main_bp)
+    app.register_blueprint(auth_bp)
+    app.register_blueprint(currencies_bp)
+    app.register_blueprint(stockmarket_bp)
+    app.register_blueprint(journey_bp)   
 
-    # Inne moduły, np. weather, można inicjalizować później
     from serwis_info.modules.weather import create_weather_blueprint
     weather_bp = create_weather_blueprint()
     app.register_blueprint(weather_bp, url_prefix='/weather')
+    
+    @app.route("/")
+    def index():
+        return render_template("index.html")
+
     return app
-
-
