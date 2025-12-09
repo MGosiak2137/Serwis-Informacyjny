e0d44d5857512f2f024f7f7c3900b68a007dcc55
Marlena <marlenaw@student.agh.edu.pl>
2025-12-09 02:59:28 +0100

    pobieranie danych dotyczących zalogowanych uzytkownikow

--- serwis_info/modules/weather/static/js/user.js
+++ serwis_info/modules/weather/static/js/user.js
@@ -4,8 +4,10 @@ export function getCookie(name) {
   if (parts.length === 2) return parts.pop().split(";").shift();
 }
 
-export const username = getCookie("username") || "user_demo";
+// ZAMIANA user_demo → null
+export const username = getCookie("username"); 
 
 export function initUserDisplay() {
+  if (!username) return; // jeśli nie ma ciasteczka, nie wyświetlaj lub ukryj
   document.getElementById("usernameDisplay").innerText = username;
 }
