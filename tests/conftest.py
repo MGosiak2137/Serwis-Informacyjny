bf5d2c7f23f31d138c63ee22e7fa15f88bb33f44
Marlena <marlenaw@student.agh.edu.pl>
2026-01-10 02:34:09 +0100

    efg

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,5 +1,10 @@
 # tests/conftest.py
 import pytest
+import sys
+import os
+ROOT_DIR = os.path.abspath(os.path.join(os.path.dirname(__file__), ".."))
+if ROOT_DIR not in sys.path:
+    sys.path.insert(0, ROOT_DIR)
 from app import create_app, db as _db
 from app.models import User
 from werkzeug.security import generate_password_hash
